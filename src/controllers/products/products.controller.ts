45e3ff57252963a78f223f63aa941b762def6cf0
Migue Martelo <miguemartelo1031@gmail.com>
2021-05-17 11:45:14 -0500

    Injectable services in product.

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -9,17 +9,22 @@ import {
   Query,
 } from '@nestjs/common';
 
+import { ProductsService } from '../../services/products/products.service';
+
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
   @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return {
-      message: `Products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
-    };
+    // return {
+    //   message: `Products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    // };
+    return this.productsService.findAll();
   }
 
   @Get('filter')
@@ -30,32 +35,36 @@ export class ProductsController {
   }
 
   @Get(':productId')
-  getProduct(@Param('productId') productId: string) {
-    return {
-      message: `Product ${productId}`,
-    };
+  getProduct(@Param('productId') productId: number) {
+    // return {
+    //   message: `Product ${productId}`,
+    // };
+    return this.productsService.findOne(productId);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: `Create action`,
-      payload,
-    };
+    // return {
+    //   message: `Create action`,
+    //   payload,
+    // };
+    return this.productsService.create(payload);
   }
 
   @Put(':productId')
-  update(@Param('productId') productId: string, @Body() payload: any) {
-    return {
-      productId,
-      payload,
-    };
+  update(@Param('productId') productId: number, @Body() payload: any) {
+    // return {
+    //   productId,
+    //   payload,
+    // };
+    return this.productsService.update(productId, payload);
   }
 
   @Delete(':productId')
-  delete(@Param('productId') productId: string) {
-    return {
-      productId,
-    };
+  delete(@Param('productId') productId: number) {
+    // return {
+    //   productId,
+    // };
+    return this.productsService.delete(productId);
   }
 }
